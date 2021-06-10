25001d5b39cec25c0f20a8b597dcb81944ed9f53
upgradeQ <18361586+upgradeQ@users.noreply.github.com>
2021-06-11 01:09:41 +0300

    Add web rendered trail,remove crop

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-Fork by 3_4_700, original by upgradeQ
-The main purpose of this fork is to be able to make a precise custom cursor, like a handdrawn arm holding a pen for artists
-
 # OBS Studio Cursor skin
 Selected source will  follow mouse pointer.
 Using [`obs_sceneitem_set_pos`](https://obsproject.com/docs/reference-scenes.html#c.obs_sceneitem_set_pos) 
@@ -11,16 +8,15 @@ Using [`obs_sceneitem_set_pos`](https://obsproject.com/docs/reference-scenes.htm
 # Limitations
 - Multiple monitors setup will not work .
 - If used in fullscreen apps, offset will appear.
-- Current code only works for 1920x1080 res
 # Usage
 - Create a _source_ with desired cursor(e.g Image source or Media source).
 - In scripts select _that_ source name.
-- Make a group, add Display Capture, Window Capture
+- Make a group, add Display Capture, Window Capture.
 
 ![img](https://i.imgur.com/CHuLwmp.png)
 
 - To crop, crop the _group_, the size should still have the same ratio as your monitor even if you scale it
-- To set offset/calibrate, use the Display Capture to see mouse and adjust it at Scripts (or use Tab/Shift+tab to navigate if in Window Capture to not move mouse). You have to do this every time you change the Group scale/move the Group
+- To set offset/calibrate, use the Display Capture to see mouse and adjust it at Scripts (or use Tab/Shift+tab to navigate, if in Window Capture, to not move mouse). You have to do this every time you change the Group scale/move it
 
 ![img](https://user-images.githubusercontent.com/66927691/121442471-56133280-c9be-11eb-9bb4-ad12b2e4ebfb.jpg)
 
@@ -28,14 +24,11 @@ Using [`obs_sceneitem_set_pos`](https://obsproject.com/docs/reference-scenes.htm
 
 
 - Test it: press Start, press Stop, tweak refresh rate.
-# Crop auto update
-Zoom or higlight.
-- Create 2 display captures.
-- Create crop filter with this name: `cropXY`.
-- Check relative.
-- Set Width and Height to relatively small numbers e.g : 64x64 .
-- Image mask blend + color correction might be an option too.
-- Run script,select this source as cursor source , check Update crop, click start.
+
+# Web rendered mouse cursor trails
+- Add browser source with mouse tracking local or online web page.
+- Make sure to set resolution as your monitor (base)
+- Fill all entries, check `Use browser source`
 
 # Zoom
 > Have you ever needed to zoom in on your screen to show some fine detail work,
@@ -53,6 +46,16 @@ They all have some level of transparency.
 ![img](https://i.imgur.com/8qoRU3i.png)
 - green circle
 ![Imgur](https://i.imgur.com/s3jvZP5.png) 
+
+# On the Roadmap
+- Visual indicator of mouse up/down state.
+- Lua based shaders rendering (on mouse up, down, trail, etc...)
+- Custom web page rendering (on mouse up, down, trail, etc...)
+
+# Acknowledgments
+- [`3_4_700`](https://github.com/34700) added offsets functionality for precise custom cursor(like a hand drawn arm holding a pen for artists)
+- [`tholman/cursor-effects`](https://github.com/tholman/cursor-effects) - stock cursor trails
+
 # Contribute
  [Forks](https://help.github.com/articles/fork-a-repo) are a great way to contribute to a repository.
 After forking a repository, you can send the original author a [pull request](https://help.github.com/articles/using-pull-requests)
--- mouse_skin_obs.py
+++ mouse_skin_obs.py
@@ -1,11 +1,124 @@
-import obspython as obs
+import obspython as S  # studio
+from contextlib import contextmanager, ExitStack
+from types import SimpleNamespace as dot
 from pynput.mouse import Controller  # python -m pip install pynput
 
+__version__ = "2.0.0"
 c = Controller()
 get_position = lambda: c.position
 
 
-__version__ = "1.0.0"
+@contextmanager
+def source_auto_release(source_name):
+    source = S.obs_get_source_by_name(source_name)
+    try:
+        yield source
+    finally:
+        S.obs_source_release(source)
+
+
+@contextmanager
+def data_ar(source_settings=None):
+    if source_settings is not None:
+        settings = S.obs_data_create()
+    else:
+        settings = S.obs_source_get_settings(source_settings)
+    try:
+        yield settings
+    finally:
+        S.obs_data_release(settings)
+
+
+@contextmanager
+def scene_from_source_ar(source):
+    source = S.obs_scene_from_source(source)
+    try:
+        yield source
+    finally:
+        S.obs_scene_release(source)
+
+
+def get_modifiers(key_modifiers):
+    if key_modifiers:
+        shift = key_modifiers.get("shift")
+        control = key_modifiers.get("control")
+        alt = key_modifiers.get("alt")
+        command = key_modifiers.get("command")
+    else:
+        shift = control = alt = command = 0
+    modifiers = 0
+
+    if shift:
+        modifiers |= S.INTERACT_SHIFT_KEY
+    if control:
+        modifiers |= S.INTERACT_CONTROL_KEY
+    if alt:
+        modifiers |= S.INTERACT_ALT_KEY
+    if command:
+        modifiers |= S.INTERACT_COMMAND_KEY
+    return modifiers
+
+
+def send_mouse_click_to_browser(
+    source,
+    x=0,
+    y=0,
+    button_type=S.MOUSE_LEFT,
+    mouse_up=False,
+    click_count=1,
+    key_modifiers=None,
+):
+    event = S.obs_mouse_event()
+    event.modifiers = get_modifiers(key_modifiers)
+    event.x = x
+    event.y = y
+    S.obs_source_send_mouse_click(source, event, button_type, mouse_up, click_count)
+
+
+def send_mouse_move_to_browser(
+    source,
+    x=0,
+    y=0,
+    key_modifiers=None,
+):
+    event = S.obs_mouse_event()
+    event.modifiers = get_modifiers(key_modifiers)
+    event.x = x
+    event.y = y
+    S.obs_source_send_mouse_move(source, event, False)  # do not leave
+
+
+G = dot()
+
+G.LMB = G.RMB = G.MOUSE_HOOKED = False
+
+
+def HTK_1_CB(pressed):
+    G.LMB = pressed
+
+
+def HTK_2_CB(pressed):
+    G.RMB = pressed
+
+
+def hook_mouse_buttons():
+    if G.MOUSE_HOOKED:
+        raise RuntimeError("already hooked mouse")
+    key_1 = '{"htk_1_mouse": [ { "key": "OBS_KEY_MOUSE1" } ], '
+    key_2 = '"htk_2_mouse": [ { "key": "OBS_KEY_MOUSE2" } ]}'
+    json_s = key_1 + key_2
+    default_hotkeys = [
+        dot(id="htk_1_mouse", des="LMB state", callback=HTK_1_CB),
+        dot(id="htk_2_mouse", des="RMB state", callback=HTK_2_CB),
+    ]
+    settings = S.obs_data_create_from_json(json_s)
+    for k in default_hotkeys:
+        a = S.obs_data_get_array(settings, k.id)
+        h = S.obs_hotkey_register_frontend(k.id, k.des, k.callback)
+        S.obs_hotkey_load(h, a)
+        S.obs_data_array_release(a)
+    S.obs_data_release(settings)
+    G.MOUSE_HOOKED = True
 
 
 def apply_scale(x, y, width, height):
@@ -13,97 +126,78 @@ def apply_scale(x, y, width, height):
     height = round(height * y)
     return width, height
 
+
 def lerp(minVal, maxVal, k):
-    val = minVal + ((maxVal - minVal)*k)
+    val = minVal + ((maxVal - minVal) * k)
     return val
+
 
 class CursorAsSource:
     source_name = None
+    target_name = None
+    browser_source_name = None
     lock = True
     flag = True
     refresh_rate = 15
+    width = 1920
+    height = 1080
+    is_update_browser = False
 
-    def update_cursor(self):
-        source = obs.obs_get_source_by_name(self.source_name)
-        settings = obs.obs_data_create()
+    def update_cursor_on_scene(self):
+        ctx = ExitStack().enter_context
+        source = ctx(source_auto_release(self.source_name))
+        settings = ctx(data_ar())
         if source is not None:
-            scene_source = obs.obs_frontend_get_current_scene()
-            scene_width = obs.obs_source_get_width(source)
-            scene_height = obs.obs_source_get_height(source)
-            scene = obs.obs_scene_from_source(scene_source)
-            scene_item = obs.obs_scene_find_source(scene, self.source_name)
-            target_item = obs.obs_scene_find_source(scene, self.target_name)
+            scene_width = S.obs_source_get_width(source)
+            scene_height = S.obs_source_get_height(source)
+
+            scene_source = S.obs_frontend_get_current_scene()
+            scene = ctx(scene_from_source_ar(scene_source))
+            scene_item = S.obs_scene_find_source_recursive(scene, self.source_name)
+            target_item = S.obs_scene_find_source_recursive(scene, self.target_name)
             if scene_item:
-                scale = obs.vec2()
-                obs.obs_sceneitem_get_scale(scene_item, scale)
+                scale = S.vec2()
+                S.obs_sceneitem_get_scale(scene_item, scale)
                 scene_width, scene_height = apply_scale(
                     scale.x, scale.y, scene_width, scene_height
                 )
-                
-                next_pos = obs.vec2()
+
+                next_pos = S.vec2()
                 next_pos.x, next_pos.y = get_position()
                 next_pos.x -= self.offset_x
                 next_pos.y -= self.offset_y
-                ## base: 1920*1080, i should add something to make this automatically change based on the Desktop Capture used
-                ## maybe make it able to use multiple monitors as well?
-                ratio_x = next_pos.x/1920
-                ratio_y = next_pos.y/1080
-                
-                target_scale = obs.vec2()
-                target = obs.obs_get_source_by_name(self.target_name)
-                obs.obs_sceneitem_get_scale(target_item, target_scale)
-                target_x = obs.obs_source_get_width(target) * target_scale.x 
-                target_y = obs.obs_source_get_height(target) * target_scale.y
-                
+                ## TODO maybe make it able to use multiple monitors as well?
+                ratio_x = next_pos.x / self.width
+                ratio_y = next_pos.y / self.height
+
+                target_scale = S.vec2()
+                target = ctx(source_auto_release(self.target_name))
+                S.obs_sceneitem_get_scale(target_item, target_scale)
+                target_x = S.obs_source_get_width(target) * target_scale.x
+                target_y = S.obs_source_get_height(target) * target_scale.y
+
                 next_pos.x = lerp(0, target_x, ratio_x)
                 next_pos.y = lerp(0, target_y, ratio_y)
-                obs.obs_sceneitem_set_pos(scene_item, next_pos)
+                S.obs_sceneitem_set_pos(scene_item, next_pos)
 
-            obs.obs_data_release(settings)
-            obs.obs_scene_release(scene)
-            obs.obs_source_release(source)
+    def update_cursor_inside_browser_source(self):
+        with source_auto_release(self.browser_source_name) as source:
+            send_mouse_move_to_browser(source, *get_position())
 
-    def update_crop(self):
-        """
-        Create 2 display captures.
-        Create crop filter with this name: cropXY.
-        Check relative.
-        Set Width and Height to relatively small numbers e.g : 64x64 .
-        Image mask blend + color correction might be an option too.
-        Run script,select this source as cursor source , check Update crop, click start.
-        """
-        source = obs.obs_get_source_by_name(self.source_name)
-        crop = obs.obs_source_get_filter_by_name(source, "cropXY")
-        filter_settings = obs.obs_source_get_settings(crop)
-
-        _x, _y = get_position()
-        # https://github.com/obsproject/obs-studio/blob/79981889c6d87d6e371e9dc8fcaad36f06eb9c9e/plugins/obs-filters/crop-filter.c#L87-L93
-        w = obs.obs_data_get_int(filter_settings, "cx")
-        h = obs.obs_data_get_int(filter_settings, "cy")
-        h, w = int(h / 2), int(w / 2)
-        obs.obs_data_set_int(filter_settings, "left", _x - h)
-        obs.obs_data_set_int(filter_settings, "top", _y - w)
-
-        obs.obs_source_update(crop, filter_settings)
-
-        obs.obs_data_release(filter_settings)
-        obs.obs_source_release(source)
-        obs.obs_source_release(crop)
-
-    def ticker(self):
-        """ how fast update.One callback at time with lock"""
+    def ticker(self):  # it is not a thread because obs might not close properly
+        """how fast update.One callback at time with lock"""
         if self.lock:
-            if self.update_xy:
-                self.update_crop()
-                self.update_cursor()
-            else:
-                self.update_cursor()
-
-        if not self.lock:
-            obs.remove_current_callback()
+            if self.is_update_browser:
+                self.update_cursor_inside_browser_source()
+                return
+            self.update_cursor_on_scene()
+        else:
+            S.remove_current_callback()
 
 
 py_cursor = CursorAsSource()
+hook_mouse_buttons()
+###############               ###############               ###############
 
 
 def stop_pressed(props, prop):
@@ -112,67 +206,84 @@ def stop_pressed(props, prop):
 
 
 def start_pressed(props, prop):
-    if py_cursor.source_name != "" and py_cursor.flag:
-        obs.timer_add(py_cursor.ticker, py_cursor.refresh_rate)
+    if py_cursor.flag:
+        S.timer_add(py_cursor.ticker, py_cursor.refresh_rate)
     py_cursor.lock = True
     py_cursor.flag = False  # to keep only one timer callback
 
 
 def script_defaults(settings):
-    obs.obs_data_set_default_int(settings, "_refresh_rate", py_cursor.refresh_rate)
+    S.obs_data_set_default_int(settings, "_refresh_rate", py_cursor.refresh_rate)
+    S.obs_data_set_default_int(settings, "_width", py_cursor.width)
+    S.obs_data_set_default_int(settings, "_height", py_cursor.height)
 
 
 def script_update(settings):
-    py_cursor.update_xy = obs.obs_data_get_bool(settings, "bool_yn")
-    py_cursor.source_name = obs.obs_data_get_string(settings, "source")
-    py_cursor.target_name = obs.obs_data_get_string(settings, "target")
-    py_cursor.refresh_rate = obs.obs_data_get_int(settings, "_refresh_rate")
-    py_cursor.offset_x = obs.obs_data_get_int(settings, "_offset_x")
-    py_cursor.offset_y = obs.obs_data_get_int(settings, "_offset_y")
+    py_cursor.source_name = S.obs_data_get_string(settings, "source")
+    py_cursor.target_name = S.obs_data_get_string(settings, "target")
+    py_cursor.refresh_rate = S.obs_data_get_int(settings, "_refresh_rate")
+    py_cursor.offset_x = S.obs_data_get_int(settings, "_offset_x")
+    py_cursor.offset_y = S.obs_data_get_int(settings, "_offset_y")
+
+    py_cursor.width = S.obs_data_get_int(settings, "_width")
+    py_cursor.width = S.obs_data_get_int(settings, "_height")
+    py_cursor.browser_source_name = S.obs_data_get_string(settings, "browser")
+    py_cursor.is_update_browser = S.obs_data_get_bool(settings, "_is_update_browser")
 
 
 def script_properties():
-    props = obs.obs_properties_create()
-    number = obs.obs_properties_add_int(
+    props = S.obs_properties_create()
+    number = S.obs_properties_add_int(
         props, "_refresh_rate", "Refresh rate (ms)", 15, 300, 5
     )
     ## i am only winging this so please forgive me
-    offsetx = obs.obs_properties_add_int(
-        props, "_offset_x", "Offset X", -5000, 5000, 1
-    )
-    offsety = obs.obs_properties_add_int(
-        props, "_offset_y", "Offset Y", -5000, 5000, 1
-    )
-    
-    p1 = obs.obs_properties_add_list(
+    S.obs_properties_add_int(props, "_offset_x", "Offset X", -5000, 5000, 1)
+    S.obs_properties_add_int(props, "_offset_y", "Offset Y", -5000, 5000, 1)
+
+    S.obs_properties_add_int(props, "_width", "base width", 1, 99999, 1)
+    S.obs_properties_add_int(props, "_height", "base height", 1, 99999, 1)
+
+    p1 = S.obs_properties_add_list(
         props,
         "source",
         "Select cursor source",
-        obs.OBS_COMBO_TYPE_EDITABLE,
-        obs.OBS_COMBO_FORMAT_STRING,
+        S.OBS_COMBO_TYPE_EDITABLE,
+        S.OBS_COMBO_FORMAT_STRING,
     )
-    p2 = obs.obs_properties_add_list(
+    p2 = S.obs_properties_add_list(
         props,
         "target",
         "Select target window",
-        obs.OBS_COMBO_TYPE_EDITABLE,
-        obs.OBS_COMBO_FORMAT_STRING,
+        S.OBS_COMBO_TYPE_EDITABLE,
+        S.OBS_COMBO_FORMAT_STRING,
     )
-    sources = obs.obs_enum_sources()
+
+    S.obs_properties_add_bool(props, "_is_update_browser", "Use browser source")
+    p3 = S.obs_properties_add_list(
+        props,
+        "browser",
+        "Select browser source",
+        S.OBS_COMBO_TYPE_EDITABLE,
+        S.OBS_COMBO_FORMAT_STRING,
+    )
+
+    sources = S.obs_enum_sources()
     if sources is not None:
-        ## property 1 for image source
         for source in sources:
-            source_id = obs.obs_source_get_unversioned_id(source)
-            name = obs.obs_source_get_name(source)
-            obs.obs_property_list_add_string(p1, name, name)
-        ## property 2 for target window    
+            source_id = S.obs_source_get_unversioned_id(source)
+            name = S.obs_source_get_name(source)
+            S.obs_property_list_add_string(p1, name, name)
         for target in sources:
-            source_id = obs.obs_source_get_unversioned_id(target)
-            name = obs.obs_source_get_name(target)
-            obs.obs_property_list_add_string(p2, name, name)
-            
-        obs.source_list_release(sources)
-    obs.obs_properties_add_button(props, "button", "Stop", stop_pressed)
-    obs.obs_properties_add_button(props, "button2", "Start", start_pressed)
-    obs.obs_properties_add_bool(props, "bool_yn", "Update crop")
+            source_id = S.obs_source_get_unversioned_id(target)
+            name = S.obs_source_get_name(target)
+            S.obs_property_list_add_string(p2, name, name)
+        for b in sources:
+            source_id = S.obs_source_get_unversioned_id(b)
+            if source_id == "browser_source":
+                name = S.obs_source_get_name(b)
+                S.obs_property_list_add_string(p3, name, name)
+
+        S.source_list_release(sources)
+    S.obs_properties_add_button(props, "button", "Stop", stop_pressed)
+    S.obs_properties_add_button(props, "button2", "Start", start_pressed)
     return props
